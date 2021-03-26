f93f84e35a688dd0f76f4289b2ae51bfc89b8bbe
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-03-26 23:16:21 +0000

    Initial solution for qualification -> MoonsAndUmbrellas

--- qualification/MoonsAndUmbrellas/Solution.java
+++ qualification/MoonsAndUmbrellas/Solution.java
@@ -7,8 +7,29 @@ public class Solution {
         final int count = Integer.parseInt(in.nextLine());
 
         for (int i = 0; i < count; i++) {
-            final var line = in.nextLine();
-            System.out.println(line);
+            final var els = in.nextLine().split(" ");
+
+            final var cjCost = Integer.parseInt(els[0]);
+            final var jcCost = Integer.parseInt(els[1]);
+            final var mural = els[2];
+
+            byte last = 0;
+            int cost = 0;
+
+            for (var pic : mural.getBytes()) {
+                if (pic == '?') {
+                    pic = last;
+                }
+
+                if (pic == 'C' && last == 'J') {
+                    cost += jcCost;
+                } else if (pic == 'J' && last == 'C') {
+                    cost += cjCost;
+                }
+                last = pic;
+            }
+
+            System.out.println(String.format("Case #%d: %d", i + 1, cost));
         }
 
         in.close();
